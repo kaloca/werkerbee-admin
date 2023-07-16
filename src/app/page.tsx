0b153d5acc51d2820becabb9f764461dfc4f94e6
Gabriel Noya <mega.kaloca@gmail.com>
2023-07-17 02:32:27 +0300

    temp fix /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import { useRouter } from 'next/navigation'
 
 export default function Home() {
 	const router = useRouter()
-	router.push('/dashboard')
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-10'>
 			<h1>Werkerbee Admin</h1>
+			<button onClick={() => router.push('/dashboard')}>Start</button>
 		</main>
 	)
 }
