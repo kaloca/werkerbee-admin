8068b5e3fc490d6cab63c057dba2569ab1bb0dcd
Gabriel Noya <mega.kaloca@gmail.com>
2023-08-04 22:49:44 +0300

    improved main screen layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,14 @@ import { useRouter } from 'next/navigation'
 export default function Home() {
 	const router = useRouter()
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-10'>
+		<main className='flex min-h-screen text-2xl font-semibold flex-col items-center justify-start p-10'>
 			<h1>Werkerbee Admin</h1>
-			<button onClick={() => router.push('/dashboard')}>Start</button>
+			<button
+				className='px-4	mt-2 py-2 rounded-md bg-sky-200 text-sky-600 font-semibold hover:bg-sky-300 hover:cursor-pointer'
+				onClick={() => router.push('/dashboard')}
+			>
+				Start
+			</button>
 		</main>
 	)
 }
