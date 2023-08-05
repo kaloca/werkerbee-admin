985c9b7b5939d36d2e02f27c30f43657415c6a4f
Gabriel Noya <mega.kaloca@gmail.com>
2023-08-05 15:52:54 +0300

    root page layout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 		<main className='flex min-h-screen text-2xl font-semibold flex-col items-center justify-start p-10'>
 			<h1>Werkerbee Admin</h1>
 			<button
-				className='px-4	mt-2 py-2 rounded-md bg-sky-200 text-sky-600 font-semibold hover:bg-sky-300 hover:cursor-pointer'
+				className='px-4	mt-2 py-2 text-sm rounded-md bg-indigo-600 text-white font-medium hover:bg-indigo-300 hover:cursor-pointer'
 				onClick={() => router.push('/dashboard')}
 			>
 				Start
